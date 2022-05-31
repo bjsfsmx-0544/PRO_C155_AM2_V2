3d5fd6b5a0ceadc7ef2a2df401eedc13b3865104
bjsfsmx-0544 <94998868+bjsfsmx-0544@users.noreply.github.com>
2022-05-31 15:26:23 -0500

    Traducción 1.00

--- components/Collider.js
+++ components/Collider.js
@@ -1,41 +1,41 @@
-// Registering component in Collider.js
+// Registrando el componente en Collider.js
 AFRAME.registerComponent("flying-birds", {
   init: function () {
     for (var i = 1; i <= 20; i++) {
-      //id
+      // ID
       var id = `hurdle${i}`;
 
-      //position variables
+      // Variables de posición
       var posX = Math.random() * 3000 + -1000;
       var posY = Math.random() * 2 + -1;
       var posZ = Math.random() * 3000 + -1000;
 
       var position = { x: posX, y: posY, z: posZ };
 
-      //call the function
+      // Llamada de la función
       this.flyingBirds(id, position);
     }
   },
   flyingBirds: (id, position) => {
-    //Get the terrain element
+    // Obtener el elemento del terreno
     var terrainEl = document.querySelector("#terrain");
 
-    //creating the bird model entity
+    // Crear la entidad del modelo del pájaro
     var birdEl = document.createElement("a-entity");
 
-    //Setting multiple attributes
+    // Estableciendo múltiples atributos
     birdEl.setAttribute("id", id);
 
     birdEl.setAttribute("position", position);
     birdEl.setAttribute("scale", { x: 500, y: 500, z: 500 });
 
-    //set the gLTF model attribute
+    // Establecer el atributo del modelo gLTF
     birdEl.setAttribute("gltf-model", "./assets/models/flying_bird/scene.gltf");
 
-    //set animation mixer of models with animation
+    // Establecer 'animation-mixer' de los modelos con animación
     birdEl.setAttribute("animation-mixer", {});
 
-    //set the static body of the physic system
+    // Establecer el cuerpo estático del sistema físico
     birdEl.setAttribute("static-body", {
       shape: "sphere",
       sphereRadius: 5,
@@ -45,7 +45,7 @@ AFRAME.registerComponent("flying-birds", {
       elementId: `#${id}`,
     });
 
-    //append the bird entity as the child of the terrain entity
+    // Agregar la entidad del pájaro como hija de la entidad del terreno
     terrainEl.appendChild(birdEl);
   },
 });
